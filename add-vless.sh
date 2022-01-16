f6125841e7146180082dc9c0c3b6ec44dd4424a2
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-01-16 15:46:25 +0700

    Update add-vless.sh

--- add-vless.sh
+++ add-vless.sh
@@ -29,8 +29,8 @@ sed -i '/#tls$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vless.json
 sed -i '/#none$/a\### '"$user $exp"'\
 },{"id": "'""$uuid""'","email": "'""$user""'"' /etc/v2ray/vnone.json
-vlesslink1="vless://${uuid}@${domain}:$tls?path=/brody&security=tls&encryption=none&type=ws#${user}"
-vlesslink2="vless://${uuid}@${domain}:$none?path=/brody&encryption=none&type=ws#${user}"
+vlesslink1="vless://${uuid}@${domain}:$tls?path=/fsid&security=tls&encryption=none&type=ws#${user}"
+vlesslink2="vless://${uuid}@${domain}:$none?path=/fsid&encryption=none&type=ws#${user}"
 systemctl restart v2ray@vless
 systemctl restart v2ray@vnone
 clear
@@ -43,8 +43,8 @@ echo -e "port none TLS  : $none"
 echo -e "id             : ${uuid}"
 echo -e "Encryption     : none"
 echo -e "network        : ws"
-echo -e "path TLS       : /brody"
-echo -e "path non TLS   : /brody"
+echo -e "path TLS       : /fsid"
+echo -e "path non TLS   : /fsid"
 echo -e "Created        : $created"
 echo -e "Expired        : $exp"
 echo -e "================================="
