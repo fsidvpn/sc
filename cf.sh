4b211ddb8c7f59f9eeedcb1c475e917de5e69bc6
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-02-23 14:37:41 +0700

    Update cf.sh

--- cf.sh
+++ cf.sh
@@ -4,9 +4,9 @@ green='\e[0;32m'
 NC='\e[0m'
 clear
 apt install jq curl -y
-DOMAIN=fsind.site
+DOMAIN=autoserv.cf
 sub=$(</dev/urandom tr -dc a-z0-9 | head -c4)
-SUB_DOMAIN=${sub}.fsind.site
+SUB_DOMAIN=${sub}.autoserv.cf
 CF_ID=awaledyan@gmail.com
 CF_KEY=7c9a592ee3186932780cffb3f1b6dbfee8084
 set -euo pipefail
